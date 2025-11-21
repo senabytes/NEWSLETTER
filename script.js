7c1d251e86b875dfcdb871c9ee3bc29d4ecb9eaa
senabytes <valkiriasenas@gmail.com>
2025-11-21 16:26:12 -0300

    COMMIT 2 - JS

---
+++ script.js
@@ -0,0 +1,24 @@
+const email = document.getElementById("mail");
+const subButton = document.getElementById("subscribe");
+const errorLabel = document.querySelector(".errorLabel");
+
+const valideEmail = (emailValue) => {
+    const regex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    return regex.test(emailValue);
+};
+
+subButton.addEventListener("click", function (event) {
+    if(email.value === ""){
+        email.classList.add("error");
+        errorLabel.style.display = "block";
+        return;
+        
+    } else if(!valideEmail(email.value)){
+        email.classList.add("error");
+        errorLabel.style.display = "block";
+        return;
+    }
+
+    email.classList.remove("error");
+    errorLabel.style.display = "none";
+});
