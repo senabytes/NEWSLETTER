b2ceda94ecda1a718be6b406987ca3d9cbbcf00d
senabytes <valkiriasenas@gmail.com>
2025-11-22 13:09:40 -0300

    Ajustes no JS

--- script.js
+++ script.js
@@ -1,30 +1,35 @@
 const email = document.getElementById("mail");
 const subButton = document.getElementById("subscribe");
 const errorLabel = document.querySelector(".errorLabel");
-
+const dismissBtn = document.getElementById("dismiss");
 
 const valideEmail = (emailValue) => {
     const regex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
     return regex.test(emailValue);
 };
 
-subButton.addEventListener("click", function (event) {
-    if(email.value === ""){
-        email.classList.add("error");
-        errorLabel.style.display = "block";
-        return;
-        
-    } else if(!valideEmail(email.value)){
-        email.classList.add("error");
-        errorLabel.style.display = "block";
-        return;
-    }else{
-        window.location.href = "success.html";
 
-    }
+if (subButton) {
+    subButton.addEventListener("click", function (event) {
+        if (email.value === "") {
+            email.classList.add("error");
+            errorLabel.style.display = "block";
+            return;
+        } else if (!valideEmail(email.value)) {
+            email.classList.add("error");
+            errorLabel.style.display = "block";
+            return;
+        } else {
+            window.location.href = `success.html?email=${encodeURIComponent(email.value)}`;
+        }
 
-    email.classList.remove("error");
-    errorLabel.style.display = "none";
-});
+        email.classList.remove("error");
+        errorLabel.style.display = "none";
+    });
+}
 
-
+if (dismissBtn) {
+    dismissBtn.addEventListener("click", function () {
+        window.location.href = "index.html";
+    });
+}
--- success.js
+++
@@ -1,6 +0,0 @@
-const dismissBtn = document.getElementById("dismiss");
-
-dismissBtn.addEventListener("click", function(){
-   window.location.href = "index.html";
-
-});
